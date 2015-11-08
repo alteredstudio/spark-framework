07e9cc95f9b9600509740d706ab9dc9dca7765c9
alteredstudio <info@ever-real.com>
2015-11-07 22:16:20 -0600

    Src add

---
+++ src/js/scripts.js
@@ -0,0 +1,28 @@
+// Default JavaScript Functions and Initiations
+$(document).ready(function() {
+
+  // Functions go here...
+
+  // var body = document.body,
+  //     html = document.documentElement;
+  //
+  // var pageHeight = Math.max( body.scrollHeight, body.offsetHeight,
+  //                      html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+  var totalHeight = document.body.scrollHeight + 60;
+  $('.overlay-grid').css({'height': totalHeight + 'px'});
+
+
+  resizeDiv();
+  window.onresize = function(event) {
+    resizeDiv();
+  }
+  function resizeDiv() {
+    viewportWidth = $(window).width();
+    viewportHeight = $(window).height();
+    //vphOffset = vph - 60;
+    $('.overlay-grid').css({'width': viewportWidth + 'px'});
+  }
+
+
+}); // end document ready
